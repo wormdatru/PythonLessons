b6ed1b8a27cb8877dafa536f934f725b1bf49fbd
wormdatru <wormdatru@gmail.com>
2024-12-25 13:06:07 +0200

    lesson 2

---
+++ lists.py
@@ -0,0 +1,11 @@
+fruits = ["apple", "banana", "cherry"]
+
+# Loop through a list
+for fruit in fruits:
+    print(fruit)
+
+# Add to the list
+fruits.append("orange")
+
+# Access list elements
+print(fruits[0])  # apple
