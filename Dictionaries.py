8ed5c24ee174e9028f9200b250e07293a3483054
wormdatru <wormdatru@gmail.com>
2024-12-25 13:04:08 +0200

    lesson 1

---
+++ Dictionaries.py
@@ -0,0 +1,8 @@
+person = {"name": "Alice", "age": 25, "city": "New York"}
+
+# Access dictionary values
+print(person["name"])
+
+# Add a new key-value pair
+person["profession"] = "Engineer"
+print(person)
